361526bc7b4b8e227766a8c12bcd70b121960183
LXDragon <sinagaalex53@gmail.com>
2022-05-13 12:50:08 +0700

    Initialized footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,34 @@
+import "../App.css";
+import "../styles/footer.css";
+
 const Footer = () => {
-  return <div></div>;
+  return (
+  <div className="bg-footer w-screen text-white flex items-start text-xl font-lora grid-cols-4">
+    <a href="#">
+      <div className="w-full justify-between flex items-center">
+        <img src="/img/icons/polban.png" style={{ width: 90, height: 90 }} />
+        <p className="header-title ml2">
+          JURUSAN TEKNIK KOMPUTER DAN INFORMATIKA
+        </p>
+      </div>
+    </a>
+    <div>
+      <p className="ml2 header-title">
+        Program Studi
+      </p>
+    </div>
+    <div>
+      <p className="ml2 header-title">
+        Tentang JTK
+      </p>
+    </div>
+    <div>
+      <p className="ml2 header-title">
+        Koneksi
+      </p>
+    </div>
+  </div>
+  );
 };
 
 export default Footer;
