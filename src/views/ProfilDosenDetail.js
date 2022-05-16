5d9bca8e5e17e8c314f0836d834b737cdc2bc3b4
SalmanS <salmanshalahuddin133@gmail.com>
2022-05-16 13:43:48 +0700

    Finished staff detail page & component refactoring

--- src/views/ProfilDosenDetail.js
+++ src/views/ProfilDosenDetail.js
@@ -6,7 +6,7 @@ const ProfilDosenDetail = () => {
   return (
     <div className="w-11/12 ml-auto mr-auto mb3 mt3 flex">
       <ProfilDetailSidebar />
-      <ProfilDetailCard />
+      <ProfilDetailCard isDosen={true} />
     </div>
   );
 };
--- src/views/ProfilStaffDetail.js
+++ src/views/ProfilStaffDetail.js
@@ -1,7 +1,14 @@
 import "../App.css";
+import ProfilDetailCard from "../components/ProfilDetailCard";
+import ProfilDetailSidebar from "../components/ProfilDetailSidebar";
 
 const ProfilStaffDetail = () => {
-  return <div></div>;
+  return (
+    <div className="w-11/12 ml-auto mr-auto mb3 mt3 flex">
+      <ProfilDetailSidebar />
+      <ProfilDetailCard isDosen={false} />
+    </div>
+  );
 };
 
 export default ProfilStaffDetail;
